946ef704bfe5b22c10fef61401c8ff485d6f8b33
Jack Schneiderhan <jschnei4@stevens.edu>
2022-12-16 10:41:00 -0500

    Basic imagemagick implementation
    This is just testing that some stuff works, still need to actually add routes that make it do what we want

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fs = require('fs');
+var im = require('imagemagick');
 const axios = require('axios');
 const https = require('https');
 const configRoutes = require('./routes');
+let testImage = "./dog.jpg"
 const elasticInfo = require('./routes/config');
 
 const [elasticUrl, postMapping, userMapping] = [elasticInfo.elasticUrl, elasticInfo.postMapping, elasticInfo.userMapping];
@@ -20,6 +23,23 @@ const instance = axios.create({
 const jsonErrorHandler = (err, req, res, next) => {
     res.status(500).send({ error: err.toString() });
   }
+
+app.get('/imageIdentify', function(req, res) {
+  im.identify('dog.jpg', function(err, features){
+  if (err) throw err;
+  res.json({"images_data": features});
+  });
+});
+
+// app.get('/imageFlip', async function(req, res) {
+//   const imgBuffer = await im.convert({
+//     srcData: 'dog.jpg',
+//     rotate: 90
+//   }, function(err, retVal){
+//     if(err) throw err;
+//     res.json({"returnValue": retVal})
+//   })
+// });
 
 app.use(cors());
 app.use(express.json());
--- backend/routes/config.js
+++ backend/routes/config.js
@@ -1,9 +1,9 @@
 //edit these fields with the url, username, and password for elastic on your machine:
 module.exports = {
     serverUrl: 'http://localhost:3030', //url of the backend api
-    elasticUrl: 'https://localhost:9200',
+    elasticUrl: 'http://localhost:9200',
     username: 'elastic',
-    password: '65MeFjfRlfRjvdFXc4T2',
+    password: '6mLMNOTdRmhvb_qRrkWZ',
     userMapping: {
         mappings: {
           properties: {
