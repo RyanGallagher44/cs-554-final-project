6d91fffaba83569576eb832717e90720e133b6c2
RyanGallagher44 <51164194+RyanGallagher44@users.noreply.github.com>
2023-01-25 12:50:38 -0500

    Update config.js

--- backend/routes/config.js
+++ backend/routes/config.js
@@ -3,7 +3,7 @@ module.exports = {
     serverUrl: 'https://localhost:3030', //url of the backend api
     elasticUrl: 'https://localhost:9200',
     username: 'elastic',
-    password: '_IK+dkeLiVZJ8XcXuaCC',
+    password: 'password',
     userMapping: {
         mappings: {
           properties: {
